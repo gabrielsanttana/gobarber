80745e06425c2cba4b0c297907edd1a69ee75392
gabrielsanttana <gabriel.gsantana7@gmail.com>
2020-07-14 22:13:04 -0300

    Add initial date validation

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -1,17 +1,36 @@
 import {Router, Request, Response} from 'express';
 import {uuid} from 'uuidv4';
+import {startOfHour, parseISO, isEqual} from 'date-fns';
+
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request: Request, response: Response) => {
   const {provider, date} = request.body;
 
-  const appointment = {
+  const parsedDate = startOfHour(parseISO(date));
+
+  const isDateAlreadyBooked = appointments.find((appointment) =>
+    isEqual(appointment.date, parsedDate),
+  );
+
+  if (isDateAlreadyBooked) {
+    return response
+      .status(400)
+      .json({error: "There's already an appointment booked for this date"});
+  }
+
+  const appointment: Appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   };
 
   appointments.push(appointment);
