05a172eb437f1e3ff77f279b64ad8a51be13ef3d
gabrielsanttana <gabriel.gsantana7@gmail.com>
2020-07-22 22:09:00 -0300

    Add CreateUserService and use it on users post route

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -1,10 +1,21 @@
 import {Router, Request, Response} from 'express';
+import CreateUserService from '../services/CreateUserService';
 
 const usersRouter = Router();
 
 usersRouter.post('/', async (request: Request, response: Response) => {
   try {
-    return response.send('/users');
+    const createUserService = new CreateUserService();
+
+    const {name, email, password} = request.body;
+
+    const user = await createUserService.execute({
+      name,
+      email,
+      password,
+    });
+
+    return response.status(200).json(user);
   } catch (error) {
     return response.status(400).json({error: error.message});
   }
