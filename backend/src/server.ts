7c260c8a6e657eb007cbb7a86fd5872af1969481
gabrielsanttana <gabriel.gsantana7@gmail.com>
2020-07-25 15:12:56 -0300

    Create endpoint to return static files

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,11 +2,13 @@ import 'reflect-metadata';
 import express from 'express';
 import routes from './routes';
 import './database';
+import uploadConfig from './config/upload';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use('/files', express.static(uploadConfig.directory));
 
 app.listen(3333, () => {
   console.log('⚙️  Server is running on port 3333');
