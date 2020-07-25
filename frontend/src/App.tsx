0aa32de512cb33ed64de6a9aaa5e53f10308648f
gabrielsanttana <gabriel.gsantana7@gmail.com>
2020-07-25 17:03:57 -0300

    Add theme-color meta on index.html

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import "./global.css";
+import React from 'react';
+import './global.css';
 
-function App() {
+const App: React.FC = () => {
   return <h1>GoBarber</h1>;
-}
+};
 
 export default App;
